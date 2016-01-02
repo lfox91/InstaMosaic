25869d206d81702dd82be2da7e1b6cb657d736c1
Daniel Levin <sporks@gmail.com>
2016-01-02 15:49:41 -0800

    Mosaic api working

---
+++ server/middleware/mosaic.js
@@ -0,0 +1,71 @@
+var mos_token = "6dRNPXKXSPnOoAwCkQ9B6YtZbpwOCaXf"
+var images = ["https://s3.amazonaws.com/printmosaic-assets/api_tutorial/yellow.jpg", "https://s3.amazonaws.com/printmosaic-assets/api_tutorial/orange.jpg", "https://s3.amazonaws.com/printmosaic-assets/api_tutorial/two_flowers.jpg"]
+var prevDat =  { "token": mos_token,
+				"path": null,
+				"resolution[]": "100",
+				"tile-size": "10",
+				"border-size": "2"
+				}
+var request = require('request')
+
+
+module.exports = {
+	prev: function(imagesArray){       
+		var that = this;                 
+		// var info = req.body
+		prevDat.path = imagesArray[0];
+		request.post('https://www.printmosaic.com/api/v2/mosaics', {form: prevDat}, function callback(err, http, body){
+			if(err){
+				console.log(err)
+			}
+			console.log("SUCCESS", body)
+			var newImgs = [];
+			//parse body to get url
+			var ret = JSON.parse(body)
+			//remove first item from array
+			imagesArray = imagesArray.slice(1)
+			imagesArray.forEach(function(a){
+				newImgs.push(a.url)
+			});
+			return that.addImgs(ret.mosaic_id, newImgs)
+		})
+	},
+
+	addImgs: function(prev_url, newImgs){
+		var that = this;
+		var str = "token="+mos_token+"&"
+		newImgs.forEach(url => {
+	  		str += 'images[]=' + url + '&';
+		});
+		request.patch({
+				headers: {'content-type' : 'application/x-www-form-urlencoded'},
+				url: 'https://www.printmosaic.com/api/v2/mosaics/'+prev_url,
+				body: str
+				},
+				function callback(err, http, body){
+					if(err){
+						console.log(err)
+					}
+			console.log("SUCCESS",err, body)
+			return that.generate(prev_url);
+		})
+	},
+
+	generate: function(prev_url){
+		request.post('https://www.printmosaic.com/api/v2/mosaics/'+prev_url+'/generate_preview',
+		 {form:{"token": mos_token}}, function callback(err, http, body){
+		 	if(err){
+		 		console.error(err)
+		 	}
+		 	console.log(body);
+		 })
+		//check until it's finished
+		console.log("https://www.printmosaic.com/mosaics/"+prev_url)
+		
+		return "https://www.printmosaic.com/mosaics/"+prev_url
+	}
+
+	
+};
+
+
--- server/server.js
+++ server/server.js
@@ -3,7 +3,13 @@ var app = express();
 var $ = require('jquery');
 var request = require('request');
 var instagram = require('./middleware/instagram.js')
+var mosaic = require('./middleware/mosaic.js')
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get(/(^\/)/, instagram.getInsta, instagram.postInsta, instagram.photos, instagram.parse);
 app.get('/', 
